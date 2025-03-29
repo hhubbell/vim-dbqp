271b6fd96047723c559016e64a762dbaa1edff1f
Harry Hubbell <harry.hubbell@gmail.com>
2025-03-28 20:58:11 -0400

    support relative paths for sqlite and duckdb

--- odbcpersist/connection.py
+++ odbcpersist/connection.py
@@ -1,20 +1,26 @@
+#
+#
+#
+
+import os
 
 
 def duckdb_connection(param_str):
     import duckdb
 
-    # FIXME
+    # FIXME?
+    if param_str:
+        param_str = os.path.expanduser(param_str)
+
     return duckdb.connect(param_str)
 
 def snowflake_connection(param_str):
     import snowflake.connector
 
-    # FIXME
     return snowflake.connector.connect()
 
 def sqlite_connection(param_str):
     import sqlite3
 
-    return sqlite3.connect(param_str)
-
+    return sqlite3.connect(os.path.expanduser(param_str))
 
